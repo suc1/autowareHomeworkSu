d3214829752390ca552bb5a94ecdc52f27d021f8
suc <ytsucheng@gmail.com>
2022-05-12 14:01:00 -0700

    build & run init

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 My homework for autoware.ai
 
 ##Ch2: Add RGB to pointClound
+```
 cd ~/autowareHomeworkSu/ch2CreateMap/RGBCloudProject
 cmake .
 make
+```
--- ch2CreateMap/RGBCloudProject/lib/LidarProcess/lidarCamera.cpp
+++ ch2CreateMap/RGBCloudProject/lib/LidarProcess/lidarCamera.cpp
@@ -5,27 +5,28 @@
 
 bool CLidarCamera::Fusion(int num) {
     CTF tf;
-    if(!tf.Read("~/autowareHomeworkSu/ch2CreateMap/RGBCloudProject/config/sensors_calib_apollo.yml")) {
+    //"~/autowareHomeworkSu/" fail
+    if(!tf.Read("/home/cheng/autowareHomeworkSu/ch2CreateMap/RGBCloudProject/config/sensors_calib_apollo.yml")) {
         return false;
     }
 
     CLidar lidar;
-    if(!lidar.ReadPcd("~/autowareHomeworkSu/ch2CreateMap/RGBCloudProject/data/apollo/lidar/11673.pcd")) {
+    if(!lidar.ReadPcd("/home/cheng/autowareHomeworkSu/ch2CreateMap/RGBCloudProject/data/apollo/lidar/11673.pcd")) {
         return false;
     }
 
     CCamera front("T_vechicle_front_camera");
-    if(!front.ReadImage("~/autowareHomeworkSu/ch2CreateMap/RGBCloudProject/data/apollo/front_camera/1571647370_656743.jpg")) {
+    if(!front.ReadImage("/home/cheng/autowareHomeworkSu/ch2CreateMap/RGBCloudProject/data/apollo/front_camera/1571647370_656743.jpg")) {
         return false;
     }
 
     CCamera left("T_vechicle_left_back_camera");
-    if(!left.ReadImage("~/autowareHomeworkSu/ch2CreateMap/RGBCloudProject/data/apollo/left_back_camera/1571647370_631318.jpg")) {
+    if(!left.ReadImage("/home/cheng/autowareHomeworkSu/ch2CreateMap/RGBCloudProject/data/apollo/left_back_camera/1571647370_631318.jpg")) {
         return false;
     }
 
     CCamera right("T_vechicle_right_back_camera");
-    if(!right.ReadImage("~/autowareHomeworkSu/ch2CreateMap/RGBCloudProject/data/apollo/right_back_camera/1571647370_631298.jpg")) {
+    if(!right.ReadImage("/home/cheng/autowareHomeworkSu/ch2CreateMap/RGBCloudProject/data/apollo/right_back_camera/1571647370_631298.jpg")) {
         return false;
     }
 
